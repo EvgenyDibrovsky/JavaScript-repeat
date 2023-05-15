09fcc00a43b2a8496855fe85109a00fe7e22473a
Evgeny Dibrovsky <105384282+EvgenyDibrovsky@users.noreply.github.com>
2023-05-15 11:25:39 +0200

    Update module-3.js

--- practice-js/module-3.js
+++ practice-js/module-3.js
@@ -14,18 +14,56 @@
 // console.table(objectTest);
 
 
-const user = {
-  name: "Jacques Gluke",
-  tag: "jgluke",
-  location: {
-    country: "Jamaica",
-    city: "Ocho Rios",
-  },
-  stats: {
-    followers: 5603,
-    views: 4827,
-    likes: 1308,
-  },
-};
+// const user = {
+//   name: "Jacques Gluke",
+//   tag: "jgluke",
+//   location: {
+//     country: "Jamaica",
+//     city: "Ocho Rios",
+//   },
+//   stats: {
+//     followers: 5603,
+//     views: 4827,
+//     likes: 1308,
+//   },
+// };
 
-console.table(user);
+// const searchName = user.location.city
+// console.table(user);
+// console.log(searchName);
+
+
+// const book = {
+//     title: "The Last Kingdom",
+//     author: "Bernard Cornwell",
+//     genres: ["historical prose", "adventure"],
+//     isPublic: true,
+//     rating: 8.38,
+//     price: 123,
+//   };
+  
+//   const bookTitle = book.title;
+//   console.log(bookTitle); // 'The Last Kingdom'
+  
+//   const bookGenres = book.genres;
+//   console.log(bookGenres); // ['historical prose', 'adventurs']
+  
+//   const bookPrice = book.price;
+//   console.log(bookPrice); // undefined
+
+
+//   const user = {
+//     name: "Jacques Gluke",
+//     tag: "jgluke",
+//     location: {
+//       country: "Jamaica",
+//       city: "Ocho Rios",
+//     },
+//     hobbies: ["swiming", "music", "sci-fi"],
+//   };
+  
+//   const location = user.location;
+//   console.table(location); // Объект location
+  
+//   const country = user.location.country;
+//   console.log(country); // 'Jamaica'
